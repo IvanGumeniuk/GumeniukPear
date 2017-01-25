23344e3652935ff0c870c695ae08610fc045278d
LittleBitUser <van_gum@i.ua>
2017-01-25 12:52:30 +0200

    WelActivity lifecycle

--- app/src/main/java/com/course/threeactivitieslogregwel/WelActivity.java
+++ app/src/main/java/com/course/threeactivitieslogregwel/WelActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class WelActivity extends AppCompatActivity {
 
@@ -32,5 +33,43 @@ public class WelActivity extends AppCompatActivity {
             }
         });
 
+        Toast.makeText(this, "WelActivity OnCreate", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "WelActivity OnRestart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "WelActivity OnStart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "WelActivity OnResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "WelActivity OnPause", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "WelActivity OnStop", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "WelActivity OnDestroy", Toast.LENGTH_SHORT).show();
     }
 }
