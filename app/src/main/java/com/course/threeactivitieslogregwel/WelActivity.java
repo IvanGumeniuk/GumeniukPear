b9a6970ff0ddc2f7228444169664130ddf163408
LittleBitUser <van_gum@i.ua>
2017-01-29 21:01:25 +0200

    added floatingbutton's funcion

--- app/src/main/java/com/course/threeactivitieslogregwel/WelActivity.java
+++ app/src/main/java/com/course/threeactivitieslogregwel/WelActivity.java
@@ -1,12 +1,19 @@
 package com.course.threeactivitieslogregwel;
 
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +24,8 @@ public class WelActivity extends FragmentActivity {
     private RecyclerView recyclerView;
     private MyAdapter adapter;
     private List<RecyclerItem> listItems;
+    private FloatingActionButton floatButton;
+    private String dialogEditPressed = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +41,20 @@ public class WelActivity extends FragmentActivity {
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        floatButton = (FloatingActionButton)findViewById(R.id.float_btn);
+
+        floatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onPressFloatingButton();
+            }
+        });
+
         listItems = new ArrayList<>();
         //Generate sample data
 
-        for (int i = 0; i<10; i++) {
-            listItems.add(new RecyclerItem("Item " + (i + 1)));
+        for (int i = 0; i<5; i++) {
+            listItems.add(new RecyclerItem("Pear" + (i + 1)));
         }
 
         //Set adapter
@@ -49,6 +67,36 @@ public class WelActivity extends FragmentActivity {
         toolbar = (Toolbar)findViewById(R.id.toolbar);
 
         toolbar.inflateMenu(R.menu.search_menu);
+
+    }
+
+    public void onPressFloatingButton(){
+
+        final   AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("New item");
+
+        final EditText input = new EditText(this);
+
+        input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PERSON_NAME);
+        builder.setView(input);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                dialogEditPressed = input.getText().toString().trim();
+                listItems.add(new RecyclerItem(dialogEditPressed));
+                adapter.notifyDataSetChanged();
+                Toast.makeText(WelActivity.this, "Created new item "+dialogEditPressed, Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
 
     }
 
