bab7749926c7cdf9e5d3c7c9430972cf3ebcc099
LittleBitUser <van_gum@i.ua>
2017-01-29 16:22:16 +0200

    with popup menu

--- app/src/main/java/com/course/threeactivitieslogregwel/WelActivity.java
+++ app/src/main/java/com/course/threeactivitieslogregwel/WelActivity.java
@@ -2,18 +2,21 @@ package com.course.threeactivitieslogregwel;
 
 
 import android.content.Intent;
-import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class WelActivity extends FragmentActivity {
 
     private Toolbar toolbar;
+    private RecyclerView recyclerView;
+    private MyAdapter adapter;
+    private List<RecyclerItem> listItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +28,27 @@ public class WelActivity extends FragmentActivity {
         initToolbar();
         toolbar.setTitle(intent.getStringExtra(getString(R.string.SPFileName)));
 
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        listItems = new ArrayList<>();
+        //Generate sample data
+
+        for (int i = 0; i<10; i++) {
+            listItems.add(new RecyclerItem("Item " + (i + 1)));
+        }
+
+        //Set adapter
+        adapter = new MyAdapter(listItems, this);
+        recyclerView.setAdapter(adapter);
+
     }
 
     private void initToolbar() {
         toolbar = (Toolbar)findViewById(R.id.toolbar);
 
-        toolbar.inflateMenu(R.menu.menu);
+        toolbar.inflateMenu(R.menu.search_menu);
 
     }
 
