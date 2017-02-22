e40431ec2c7ce43bd9c42686475fda6484fbb928
LittleBitUser <van_gum@i.ua>
2017-02-22 15:15:25 +0200

    weather added

--- app/src/main/java/com/gumeniuk/pear/Activities/MainActivity.java
+++ app/src/main/java/com/gumeniuk/pear/Activities/MainActivity.java
@@ -2,13 +2,17 @@ package com.gumeniuk.pear.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.gumeniuk.pear.MyApplicationClass;
 import com.gumeniuk.pear.R;
 
@@ -17,6 +21,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button btnSingIn, btnSingUp, btnExit;
     EditText login, password;
     MyApplicationClass app;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         app = ((MyApplicationClass)getApplicationContext());
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+
+                    Intent intent = new Intent(MainActivity.this, WelcActivity.class);
+                    app.setUserLogin(user.getDisplayName());
+                    app.setLaunch(true);
+                    startActivity(intent);
+
+                    Log.d("Firebase", "onAuthStateChanged:signed_in:" + user.getUid());
+                    finish();
+                } else {
+                    // User is signed out
+                    Log.d("Firebase", "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
 
         if(app.isLogged() && !app.getEnteringLogin().equals("")){
             startWelcome();
@@ -46,6 +75,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         password = (EditText) findViewById(R.id.textPassword);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
 
     @Override
     public void onClick(View v) {
@@ -80,4 +114,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         finish();
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
 }
--- app/src/main/java/com/gumeniuk/pear/Activities/RegActivity.java
+++ app/src/main/java/com/gumeniuk/pear/Activities/RegActivity.java
@@ -2,12 +2,19 @@ package com.gumeniuk.pear.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.gumeniuk.pear.MyApplicationClass;
 import com.gumeniuk.pear.R;
 
@@ -16,6 +23,7 @@ public class RegActivity extends AppCompatActivity {
     EditText login,password,rep_password;
     Button btnRegistration, backToLogin;
     MyApplicationClass app;
+    private FirebaseAuth mAuth;
 
 
     @Override
@@ -37,6 +45,8 @@ public class RegActivity extends AppCompatActivity {
 
         app = ((MyApplicationClass)getApplicationContext());
 
+        mAuth = FirebaseAuth.getInstance();
+
         backToLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -45,12 +55,11 @@ public class RegActivity extends AppCompatActivity {
             }
         });
 
-
         btnRegistration.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 login.setText(login.getText().toString().trim());
-                if(app.checkingPerson(login,password,rep_password))
+               /* if(app.checkingPerson(login,password,rep_password))
                 {
                     app.addNewPerson(login,password);
                     app.entering(login);
@@ -59,9 +68,34 @@ public class RegActivity extends AppCompatActivity {
                     app.setLaunch(true);
                     startActivity(new Intent(RegActivity.this, WelcActivity.class));
                     finish();
+                }*/
+                if(app.checkingPerson(login,password,rep_password)){
+                    googleRegistration();
+
                 }
+
             }
         });
 
+    }
+
+    private void googleRegistration(){
+        mAuth.createUserWithEmailAndPassword(login.getText().toString().trim(), password.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("Firebase_Reg", "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(RegActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
     }
 }
